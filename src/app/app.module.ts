719fc48871b55552cc44ca3665fde797d2b61aec
Mateusz Glejzer <mateusz.glejzer@fortedigital.no>
2023-05-03 15:32:22 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskBoardComponent } from './tasks/task-board/task-board.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { TaskInfoComponent } from './tasks/task-info/task-info.component';
+import { ProjectInfoComponent } from './project/project-info/project-info.component';
+import { ProjectEditComponent } from './project/project-edit/project-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TaskBoardComponent,
+    TaskComponent,
+    TaskInfoComponent,
+    ProjectInfoComponent,
+    ProjectEditComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tasks/task-info/task-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-info',
+  templateUrl: './task-info.component.html',
+  styleUrls: ['./task-info.component.scss']
+})
+export class TaskInfoComponent {
+
+}
